ac925a5ce542310c03e0a29427bdf1d1d1cd2d42
Homayoon Parsaei <Homayoon.parsaei@gmail.com>
2024-10-02 00:11:32 -0500

    Added C to K button.

--- TempConvertor/FormMain.cs
+++ TempConvertor/FormMain.cs
@@ -10,17 +10,27 @@ namespace GitVersionControl
         private void buttonFtoC_Click(object sender, EventArgs e)
         {
             textBoxTempC.Text = ((float.Parse(textBoxTempF.Text) - 32) * 5 / 9).ToString();
+            labelTempInF.Text = "Temperature °F";
         }
 
         private void buttonCtoF_Click(object sender, EventArgs e)
         {
             textBoxTempF.Text = ((float.Parse(textBoxTempC.Text) * 9 / 5) + 32).ToString();
+            labelTempInF.Text = "Temperature °F";
         }
 
         private void buttonClear_Click(object sender, EventArgs e)
         {
             textBoxTempF.Text = string.Empty;
             textBoxTempC.Text = string.Empty;
+            labelTempInF.Text = "Temperature °F";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            labelTempInF.Text = "Temperature °K";
+            textBoxTempF.Text = (float.Parse(textBoxTempC.Text) +273.15).ToString();
             
         }
     }
